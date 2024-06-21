0eea16fd949202f9fee9e03d76cfb64ec91bdb6d
Tororoi <cantwell.tom@gmail.com>
2024-06-20 20:25:17 -0700

    calculator division digits overflow

--- calculator.c
+++ calculator.c
@@ -7,7 +7,8 @@ int main(void)
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-    printf("%i\n", add(x, y));
+    double z = (double)x / (double)y;
+    printf("%.20f\n", z);
 }
 
 int add(int a, int b)
