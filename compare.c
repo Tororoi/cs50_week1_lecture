640acfbacf3bb549bd6d453b63b31fa50d8614db
Tororoi <cantwell.tom@gmail.com>
2024-06-20 18:30:27 -0700

    create meow program

--- compare.c
+++ compare.c
@@ -3,19 +3,19 @@
 
 int main(void)
 {
-  int x = get_int("What's x? ");
-  int y = get_int("What's y? ");
+    int x = get_int("What's x? ");
+    int y = get_int("What's y? ");
 
-  if (x < y)
-  {
-    printf("x is less than y\n");
-  }
-  else if (x > y)
-  {
-    printf("x is greater than y\n");
-  }
-  else
-  {
-    printf("x is equal to y\n");
-  }
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("x is equal to y\n");
+    }
 }
