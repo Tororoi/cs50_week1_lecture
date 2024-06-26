0009d6541ef94169d38714f7a16ceffd48ad5359
Tororoi <cantwell.tom@gmail.com>
2024-06-26 16:43:12 -0700

    demonstration of string as array of chars

--- week_2/hi.c
+++ week_2/hi.c
@@ -1,10 +1,8 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-
-    printf("%i%i%i\n", c1, c2, c3);
+    string s = "HI!";
+    printf("%c%c%c\n", s[0], s[1], s[2]);
 }
